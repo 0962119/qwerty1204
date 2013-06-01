58c7606efc50028b1cc69489c3699e3943497de0
httam0209@gmail.com <httam0209@gmail.com@fa3ea844-0d1e-1ea7-d3d8-793ef1e3e161>
2013-06-01 13:54:06 +0000

    Design lại giao diện xíu' Dang làm các xử lý thực đợn' Commit giao diện lên trước để sau nhóm hướng theo đó mà làm, chuẩn hok cần chỉnh lại

---
+++ trunk/Restaurant/Restaurant/FormThemLoaiThucDon.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Restaurant
+{
+    partial class FormThemLoaiThucDon
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormThemLoaiThucDon));
+            this.labelX1 = new DevComponents.DotNetBar.LabelX();
+            this.txtLoaiThucDon = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.btnHuyboLoaiTD = new DevComponents.DotNetBar.ButtonX();
+            this.btnDongYLoaiTD = new DevComponents.DotNetBar.ButtonX();
+            this.SuspendLayout();
+            // 
+            // labelX1
+            // 
+            this.labelX1.BackColor = System.Drawing.Color.White;
+            // 
+            // 
+            // 
+            this.labelX1.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelX1.ForeColor = System.Drawing.Color.Black;
+            this.labelX1.Location = new System.Drawing.Point(49, 22);
+            this.labelX1.Name = "labelX1";
+            this.labelX1.Size = new System.Drawing.Size(157, 23);
+            this.labelX1.TabIndex = 0;
+            this.labelX1.Text = "Nhập Tên Loại Thực Đơn:";
+            // 
+            // txtLoaiThucDon
+            // 
+            this.txtLoaiThucDon.BackColor = System.Drawing.Color.White;
+            // 
+            // 
+            // 
+            this.txtLoaiThucDon.Border.Class = "TextBoxBorder";
+            this.txtLoaiThucDon.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.txtLoaiThucDon.ForeColor = System.Drawing.Color.Black;
+            this.txtLoaiThucDon.Location = new System.Drawing.Point(49, 52);
+            this.txtLoaiThucDon.Multiline = true;
+            this.txtLoaiThucDon.Name = "txtLoaiThucDon";
+            this.txtLoaiThucDon.Size = new System.Drawing.Size(268, 26);
+            this.txtLoaiThucDon.TabIndex = 1;
+            // 
+            // btnHuyboLoaiTD
+            // 
+            this.btnHuyboLoaiTD.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.btnHuyboLoaiTD.BackColor = System.Drawing.Color.Chocolate;
+            this.btnHuyboLoaiTD.ColorTable = DevComponents.DotNetBar.eButtonColor.Orange;
+            this.btnHuyboLoaiTD.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnHuyboLoaiTD.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHuyboLoaiTD.Location = new System.Drawing.Point(173, 84);
+            this.btnHuyboLoaiTD.Name = "btnHuyboLoaiTD";
+            this.btnHuyboLoaiTD.Size = new System.Drawing.Size(75, 28);
+            this.btnHuyboLoaiTD.Symbol = "";
+            this.btnHuyboLoaiTD.SymbolSize = 13F;
+            this.btnHuyboLoaiTD.TabIndex = 2;
+            this.btnHuyboLoaiTD.Text = "Hủy bỏ";
+            this.btnHuyboLoaiTD.TextColor = System.Drawing.Color.White;
+            // 
+            // btnDongYLoaiTD
+            // 
+            this.btnDongYLoaiTD.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.btnDongYLoaiTD.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnDongYLoaiTD.ColorTable = DevComponents.DotNetBar.eButtonColor.Blue;
+            this.btnDongYLoaiTD.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDongYLoaiTD.Location = new System.Drawing.Point(92, 84);
+            this.btnDongYLoaiTD.Name = "btnDongYLoaiTD";
+            this.btnDongYLoaiTD.Size = new System.Drawing.Size(75, 28);
+            this.btnDongYLoaiTD.Symbol = "";
+            this.btnDongYLoaiTD.SymbolSize = 13F;
+            this.btnDongYLoaiTD.TabIndex = 2;
+            this.btnDongYLoaiTD.Text = "Đồng ý";
+            this.btnDongYLoaiTD.TextColor = System.Drawing.Color.White;
+            this.btnDongYLoaiTD.Click += new System.EventHandler(this.btnDongYLoaiTD_Click);
+            // 
+            // FormThemLoaiThucDon
+            // 
+            this.AcceptButton = this.btnDongYLoaiTD;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.CancelButton = this.btnHuyboLoaiTD;
+            this.ClientSize = new System.Drawing.Size(351, 121);
+            this.Controls.Add(this.btnDongYLoaiTD);
+            this.Controls.Add(this.btnHuyboLoaiTD);
+            this.Controls.Add(this.txtLoaiThucDon);
+            this.Controls.Add(this.labelX1);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormThemLoaiThucDon";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Phần Mềm Quản Lý Nhà Hàng QQDKT";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevComponents.DotNetBar.LabelX labelX1;
+        private DevComponents.DotNetBar.Controls.TextBoxX txtLoaiThucDon;
+        private DevComponents.DotNetBar.ButtonX btnHuyboLoaiTD;
+        private DevComponents.DotNetBar.ButtonX btnDongYLoaiTD;
+    }
+}
---
+++ trunk/Restaurant/Restaurant/FormWarningThemLoaiTD.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Restaurant
+{
+    partial class FormWarningThemLoaiTD
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormWarningThemLoaiTD));
+            this.buttonX1 = new DevComponents.DotNetBar.ButtonX();
+            this.labelX1 = new DevComponents.DotNetBar.LabelX();
+            this.superTooltip1 = new DevComponents.DotNetBar.SuperTooltip();
+            this.SuspendLayout();
+            // 
+            // buttonX1
+            // 
+            this.buttonX1.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.buttonX1.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.buttonX1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonX1.Location = new System.Drawing.Point(93, 39);
+            this.buttonX1.Name = "buttonX1";
+            this.buttonX1.Size = new System.Drawing.Size(75, 23);
+            this.buttonX1.Style = DevComponents.DotNetBar.eDotNetBarStyle.OfficeXP;
+            this.buttonX1.TabIndex = 0;
+            this.buttonX1.Text = "OK";
+            this.buttonX1.Click += new System.EventHandler(this.buttonX1_Click);
+            // 
+            // labelX1
+            // 
+            // 
+            // 
+            // 
+            this.labelX1.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelX1.Location = new System.Drawing.Point(63, 3);
+            this.labelX1.Name = "labelX1";
+            this.labelX1.Size = new System.Drawing.Size(140, 23);
+            this.labelX1.TabIndex = 2;
+            this.labelX1.Text = "Vui lòng nhập thông tin!";
+            // 
+            // FormWarningThemLoaiTD
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(246, 74);
+            this.Controls.Add(this.labelX1);
+            this.Controls.Add(this.buttonX1);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormWarningThemLoaiTD";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Phần Mềm Quản Lý Nhà Hàng QQDKT";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevComponents.DotNetBar.ButtonX buttonX1;
+        private DevComponents.DotNetBar.LabelX labelX1;
+        private DevComponents.DotNetBar.SuperTooltip superTooltip1;
+
+    }
+}
